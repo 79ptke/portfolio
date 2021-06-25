e780eee4b8d67702f415512b51cb556de15b9924
79ptke <82435745+79ptke@users.noreply.github.com>
2021-06-25 17:08:54 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,225 +14,22 @@ $(document).ready(function () {
             var loadedSection = this;
 
             //색인 사용
-            if (destination.index == 0) {
 
 
+            if (destination.index == 2) {
 
-            }
-
-
-            if (destination.index == 1) {
-
-
-                setTimeout(function () {
-
-
-                    setTimeout(function () {
-                        var typingBool = false;
-                        var typingIdx = 0;
-                        var liIndex = 0;
-                        var liLength = $(".typing-txt01>ul>li").length;
-
-                        // 타이핑될 텍스트를 가져온다 
-                        var typingTxt = $(".typing-txt01>ul>li").eq(liIndex).text();
-                        //liIndex 인덱스로 구분해 한줄씩 가져옴
-
-                        typingTxt = typingTxt.split(""); // 한글자씩 잘라 배열로만든다
-
-                        if (typingBool == false) { // 타이핑이 진행되지 않았다면 
-                            typingBool = true;
-                            var tyInt = setInterval(typing, 100); // 반복동작 
-                        }
-
-                        function typing() {
-                            $(".typing01 ul li").removeClass("on");
-                            $(".typing01 ul li").eq(liIndex).addClass("on");
-                            //현재 타이핑되는 문장에만 커서 애니메이션을 넣어준다.
-
-                            if (typingIdx < typingTxt.length) { // 타이핑될 텍스트 길이만큼 반복 
-                                $(".typing01 ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
-                                typingIdx++;
-                            } else { //한문장이끝나면
-                                if (liIndex < liLength - 1) {
-                                    //다음문장으로  가기위해 인덱스를 1증가
-                                    liIndex++;
-                                    //다음문장을 타이핑하기위한 셋팅
-                                    typingIdx = 0;
-                                    typingBool = false;
-                                    typingTxt = $(".typing-txt01>ul>li").eq(liIndex).text();
-
-                                    //다음문장 타이핑전 1초 쉰다
-                                    clearInterval(tyInt);
-                                    //타이핑종료
-
-                                    setTimeout(function () {
-                                        //1초후에 다시 타이핑 반복 시작
-                                        tyInt = setInterval(typing, 100);
-                                    }, 1000);
-                                } else if (liIndex == liLength - 1) {
-
-                                    //마지막 문장까지 써지면 반복종료
-                                    clearInterval(tyInt);
-
-                                    //1초후
-                                    setTimeout(function () {
-                                        //사용했던 변수 초기화
-                                        typingBool = false;
-                                        liIndex = 0;
-                                        typingIdx = -0;
-
-                                        //첫번째 문장으로 셋팅
-                                        typingTxt = $(".typing-txt01>ul>li").eq(liIndex).text();
-                                        //타이핑 결과 모두 지우기
-                                        $(".typing01 ul li").html("")
-
-                                    }, 1000);
-
-
-                                }
-                            }
-                        }
-                    }, 1000);
-                    setTimeout(function () {
-                        var typingBool = false;
-                        var typingIdx = 0;
-                        var liIndex = 0;
-                        var liLength = $(".typing-txt02>ul>li").length;
-
-                        // 타이핑될 텍스트를 가져온다 
-                        var typingTxt = $(".typing-txt02>ul>li").eq(liIndex).text();
-                        //liIndex 인덱스로 구분해 한줄씩 가져옴
-
-                        typingTxt = typingTxt.split(""); // 한글자씩 잘라 배열로만든다
-
-                        if (typingBool == false) { // 타이핑이 진행되지 않았다면 
-                            typingBool = true;
-                            var tyInt = setInterval(typing, 100); // 반복동작 
-                        }
-
-                        function typing() {
-                            $(".typing02 ul li").removeClass("on");
-                            $(".typing02 ul li").eq(liIndex).addClass("on");
-                            //현재 타이핑되는 문장에만 커서 애니메이션을 넣어준다.
-
-                            if (typingIdx < typingTxt.length) { // 타이핑될 텍스트 길이만큼 반복 
-                                $(".typing02 ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
-                                typingIdx++;
-                            } else { //한문장이끝나면
-                                if (liIndex < liLength - 1) {
-                                    //다음문장으로  가기위해 인덱스를 1증가
-                                    liIndex++;
-                                    //다음문장을 타이핑하기위한 셋팅
-                                    typingIdx = 0;
-                                    typingBool = false;
-                                    typingTxt = $(".typing-txt02>ul>li").eq(liIndex).text();
-
-                                    //다음문장 타이핑전 1초 쉰다
-                                    clearInterval(tyInt);
-                                    //타이핑종료
-
-                                    setTimeout(function () {
-                                        //1초후에 다시 타이핑 반복 시작
-                                        tyInt = setInterval(typing, 100);
-                                    }, 1000);
-                                } else if (liIndex == liLength - 1) {
-
-                                    //마지막 문장까지 써지면 반복종료
-                                    clearInterval(tyInt);
-
-                                    //1초후
-                                    setTimeout(function () {
-                                        //사용했던 변수 초기화
-                                        typingBool = false;
-                                        liIndex = 0;
-                                        typingIdx = -0;
-
-                                        //첫번째 문장으로 셋팅
-                                        typingTxt = $(".typing-txt02>ul>li").eq(liIndex).text();
-                                        //타이핑 결과 모두 지우기
-                                        $(".typing02 ul li").html("")
-
-                                    }, 1000);
-
-
-                                }
-                            }
-                        }
-                    }, 7000);
-                    setTimeout(function () {
-                        var typingBool = false;
-                        var typingIdx = 0;
-                        var liIndex = 0;
-                        var liLength = $(".typing-txt03>ul>li").length;
-
-                        // 타이핑될 텍스트를 가져온다 
-                        var typingTxt = $(".typing-txt03>ul>li").eq(liIndex).text();
-                        //liIndex 인덱스로 구분해 한줄씩 가져옴
-
-                        typingTxt = typingTxt.split(""); // 한글자씩 잘라 배열로만든다
-
-                        if (typingBool == false) { // 타이핑이 진행되지 않았다면 
-                            typingBool = true;
-                            var tyInt = setInterval(typing, 100); // 반복동작 
-                        }
-
-                        function typing() {
-                            $(".typing03 ul li").removeClass("on");
-                            $(".typing03 ul li").eq(liIndex).addClass("on");
-                            //현재 타이핑되는 문장에만 커서 애니메이션을 넣어준다.
-
-                            if (typingIdx < typingTxt.length) { // 타이핑될 텍스트 길이만큼 반복 
-                                $(".typing03 ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
-                                typingIdx++;
-                            } else { //한문장이끝나면
-                                if (liIndex < liLength - 1) {
-                                    //다음문장으로  가기위해 인덱스를 1증가
-                                    liIndex++;
-                                    //다음문장을 타이핑하기위한 셋팅
-                                    typingIdx = 0;
-                                    typingBool = false;
-                                    typingTxt = $(".typing-txt03>ul>li").eq(liIndex).text();
-
-                                    //다음문장 타이핑전 1초 쉰다
-                                    clearInterval(tyInt);
-                                    //타이핑종료
-
-                                    setTimeout(function () {
-                                        //1초후에 다시 타이핑 반복 시작
-                                        tyInt = setInterval(typing, 100);
-                                    }, 1000);
-                                } else if (liIndex == liLength - 1) {
-
-                                    //마지막 문장까지 써지면 반복종료
-                                    clearInterval(tyInt);
-
-                                    //1초후
-                                    setTimeout(function () {
-                                        //사용했던 변수 초기화
-                                        typingBool = false;
-                                        liIndex = 0;
-                                        typingIdx = -0;
-
-                                        //첫번째 문장으로 셋팅
-                                        typingTxt = $(".typing-txt03>ul>li").eq(liIndex).text();
-                                        //타이핑 결과 모두 지우기
-                                        $(".typing03 ul li").html("")
-
-                                    }, 1000);
-
-
-                                }
-                            }
-                        }
-                    }, 16500);
-
-
-                }, 1000);
+                $(".skill01").addClass("skill");
+                $(".skill02").addClass("skill");
+                $(".skill03").addClass("skill2");
+                $(".skill04").addClass("skill3");
+                $(".skill05").addClass("skill4");
 
             } else {
-
-
-
+                $(".skill01").removeClass("skill");
+                $(".skill02").removeClass("skill");
+                $(".skill03").removeClass("skill2");
+                $(".skill04").removeClass("skill3");
+                $(".skill05").removeClass("skill4");
             }
 
         },
@@ -243,16 +40,78 @@ $(document).ready(function () {
 
 
 
+    //타이핑 텍스트 960이하        
+
+    var typingWrap = false;
+    var typingIndex = 0;
+    var liIndex = 0;
+    var liLength = $(".typing-txt01>ul>li").length;
+
+    var typingText = $(".typing-txt01>ul>li").eq(liIndex).text();
+
+    typingText = typingText.split("");
+
+    if (typingWrap == false) {
+        typingWrap = true;
+        var typingInt = setInterval(typing, 100); //반복
+    }
+
+    function typing() {
+        $(".typing01 ul li").removeClass("on");
+        $(".typing01 ul li").eq(liIndex).addClass("on");
+        //커서 효과. 현재 타이핑되는 문장에만 효과가 나타난다.
+
+        if (typingIndex < typingText.length) {
+            $(".typing01 ul li").eq(liIndex).append(typingText[typingIndex]);
+            //이것만 쓰면 첫글자만 반복해서 나오므로
+            typingIndex++; //꼭 ++를 써준다. 첫문장 완성!'안녕하세요'
+        } else {
+            if (liIndex < liLength - 1) {
+                liIndex++;
+
+                typingIndex = 0;
+                typingWrap = false;
+                typingText = $(".typing-txt01>ul>li").eq(liIndex).text();
+
+                clearInterval(typingInt);
+
+                setTimeout(function () {
+                    typingInt = setInterval(typing, 100);
+                }, 1000);
+            } else if (liIndex == liLength - 1) {
+                clearInterval(typingInt);
+
+                setTimeout(function () {
+                    typingWrap = false;
+                    liIndex = 0;
+                    typingIndex = -0;
+
+                    typingText = $(".typing-txt01>ul>li").eq(liIndex).text();
+
+                }, 1000);
+            }
+        }
+    }
 
 
 
 
-    //포트폴리오 썸네일 슬라이드
+
+
+
+
+
+    //웹 포트폴리오 슬라이드
     var swiper = new Swiper(".mySwiper", {
 
-        slidesPerView: 3,
+        slidesPerView: 1,
         spaceBetween: 20,
         loop: true,
+
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
 
         navigation: {
             nextEl: ".swiper-button-next",
@@ -261,111 +120,107 @@ $(document).ready(function () {
     });
 
 
-    //포트폴리오 썸네일 클릭 시
-    $(".portfolio-page > .contents > .txt-wrap > .thumb-wrap > .thumb > .swiper-container > .swiper-wrapper > .swiper-slide").click(function () {
-
-        var contents = $(this).index() - 3;
-
-        $(".portfolio-page > .contents > .main-con > .main").removeClass("on");
-        $(".portfolio-page > .contents > .main-con > .main").eq(contents).addClass("on");
 
 
-        $(".portfolio-page > .contents > .txt-wrap > .txt").removeClass("on");
-        $(".portfolio-page > .contents > .txt-wrap > .txt").eq(contents).addClass("on");
 
+    //포폴 이미지 클릭시    
+    $(".portfolio-page02 > .contents > .main-con > .main-box > .main01").click(function () {
+        $(".img-pop-wrap").addClass("pop");
+        $(".img-pop-wrap > .img-pop > .box01").addClass("pop");
+    });
+    $(".portfolio-page02 > .contents > .main-con > .main-box > .main02").click(function () {
+        $(".img-pop-wrap").addClass("pop");
+        $(".img-pop-wrap > .img-pop > .box02").addClass("pop");
+    });
+    $(".portfolio-page02 > .contents > .main-con > .main-box > .main03").click(function () {
+        $(".img-pop-wrap").addClass("pop");
+        $(".img-pop-wrap > .img-pop > .box03").addClass("pop");
+    });
+    $(".portfolio-page02 > .contents > .main-con > .main-box > .main04").click(function () {
+        $(".img-pop-wrap").addClass("pop");
+        $(".img-pop-wrap > .img-pop > .box04").addClass("pop");
+    });
+    $(".portfolio-page02 > .contents > .main-con > .main-box > .main05").click(function () {
+        $(".img-pop-wrap").addClass("pop");
+        $(".img-pop-wrap > .img-pop > .box05").addClass("pop");
+    });
+    $(".portfolio-page02 > .contents > .main-con > .main-box > .main06").click(function () {
+        $(".img-pop-wrap").addClass("pop");
+        $(".img-pop-wrap > .img-pop > .box06").addClass("pop");
+    });
+    $(".portfolio-page02 > .contents > .main-con > .main-box > .main07").click(function () {
+        $(".img-pop-wrap").addClass("pop");
+        $(".img-pop-wrap > .img-pop > .box07").addClass("pop");
+    });
+    $(".portfolio-page02 > .contents > .main-con > .main-box > .main08").click(function () {
+        $(".img-pop-wrap").addClass("pop");
+        $(".img-pop-wrap > .img-pop > .box08").addClass("pop");
+    });
+
+
+
+    $(".img-pop-wrap").click(function () {
+
+        $(".img-pop-wrap").removeClass("pop");
+        $(".img-pop-wrap > .img-pop > .img-box").removeClass("pop");
 
     });
 
-    //포트폴리오02 썸네일 클릭 시
-    $(".portfolio-page02 > .contents > .txt-wrap > .thumb-wrap > .thumb > .swiper-container > .swiper-wrapper > .swiper-slide").click(function () {
-
-        var contents = $(this).index() - 3;
-
-        $(".portfolio-page02 > .contents > .main-con > .main").removeClass("on");
-        $(".portfolio-page02 > .contents > .main-con > .main").eq(contents).addClass("on");
 
 
-        $(".portfolio-page02 > .contents > .txt-wrap > .txt").removeClass("on");
-        $(".portfolio-page02 > .contents > .txt-wrap > .txt").eq(contents).addClass("on");
 
 
-    });
 
-
-    //960시 포트폴리오 슬라이드
-    var swiper = new Swiper(".portfolio", {
+    //960시 웹 포트폴리오 슬라이드
+    var swiper = new Swiper(".web-portfolio", {
 
         slidesPerView: 1,
         spaceBetween: 20,
         loop: true,
-        
+        keyboard: true,
+        autoplay: true,
+        speed: 500,
+
         pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
+            el: ".swiper-pagination",
+            clickable: true,
         },
 
 
 
     });
-    
-    
-    
-    
-    //모바일 메뉴 토글
-    
-     $("header > .mobile-gnb").click(function () {
-        $("header > .mobile-gnb").toggleClass("active");
+
+
+    //960시 시각 포트폴리오 슬라이드
+    var swiper = new Swiper(".portfolio", {
+
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        keyboard: true,
+        autoplay: true,
+        speed: 500,
+
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
 
 
 
     });
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+    //모바일 메뉴 토글
+
+    $("header > .mobile-gnb").click(function () {
+        $("header > .mobile-gnb").toggleClass("active");
+
+
+
+    });
 
 
 
